1a941a0730a6cffd964f24cbd964e73294f84250
marine727 <47843216+marine727@users.noreply.github.com>
2020-04-15 02:02:39 +0430

    test POST /todos rout

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,16 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+mongoose.connect('mongodb://localhost:27017/TodoApp' ,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log('DB connected!'))
+  .catch(err => {
+    console.log(`DB Connection Error: ${err.message}`);
+  });
 
 module.exports = { mongoose };
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ var { User } = require('./models/user');
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/todos' , (req , res)=>{
+app.post('/todos' , (req , res)=>{
     var todo = new Todo({
         text: req.body.text
     });
@@ -22,3 +22,5 @@ app.get('/todos' , (req , res)=>{
 app.listen(3000 , ()=>{
     console.log('Started on port 3000')
 })
+
+module.exports = { app }
